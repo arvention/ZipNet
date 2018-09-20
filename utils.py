65fab421313e750b581434ba621dbc01b27a1b76
arvention <amc.antioquia@yahoo.com.ph>
2018-09-21 00:45:08 +0800

    added use_gpu argument to method to_var

--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@ import os
 import torch
 
 
-def to_var(x, requires_grad=False):
-    if torch.cuda.is_available():
+def to_var(x, use_gpu, requires_grad=False):
+    if torch.cuda.is_available() and use_gpu:
         x = x.cuda()
     x.requires_grad = requires_grad
     return x
